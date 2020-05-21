cf10dbd846900a86e4a0bb6a78cf5e7822d72261
hbrashid <hbrashid@gmail.com>
2020-05-20 20:54:43 -0500

    Finished punk API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class Beer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLiked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      isLiked: !this.state.isLiked
+    });
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '0 auto 60px auto', width: '500px', border: '1px solid black', padding: '20px', borderRadius: '4px'}} >
+    <img style={{ width: '30px' }} src={this.props.beer.image_url} />
+    <button onClick={this.handleClick} >{this.state.isLiked ? 'Liked' : 'Like'}</button>
+    <div>{this.props.beer.name}</div>
+    <p>{this.props.beer.tagline}</p>
+    <p>{this.props.beer.description}</p>
     </div>
   );
+}
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: []
+    };
+  }
+
+  componentDidMount () {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(json => json.json())
+    .then(data => {
+      this.setState({
+        beers: data
+      })
+      console.log(data);
+    })
+  }
+
+  
+
+  render() {
+  return (
+    <div className="App">
+      {this.state.beers.map((beerData, index) => (
+      <Beer key={index} beer={beerData} />
+      ))}
+    </div>
+  );
+  }
 }
 
 export default App;
